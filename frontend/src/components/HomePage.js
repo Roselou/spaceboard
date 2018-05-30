0619d91fb887ebc5c86a4d41c6ae6348d9853e20
Roselou <Elizabethroselou@gmail.com>
2018-05-29 22:46:46 -0700

    changed h5 tags

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -14,18 +14,17 @@ class HomePage extends Component{
         return(
             <div> 
                 <Link to='/api/nasa/blackholes'><img className="col s6 icon"src={blackhole} alt="black hole icon" /></Link>
-                <h4 className="col s4" > Black Holes </h4>
+                <h5 className="col s4" > Black Holes </h5>
                 <Link to='/api/nasa/nebulae'> <img className="col s6 icon"  src={nebula} alt="nebula icon" /> </Link> 
-                <h4 className="col s4" > Nebulae </h4>
+                <h5 className="col s4" > Nebulae </h5>
                 <Link to='/api/nasa/planets'> <img className="col s6 icon" src={solarsystem} alt="planet icon" /> </Link>
-                <h4 className="col s4" > Planets </h4> 
+                <h5 className="col s4" > Planets </h5> 
                 <Link to='/api/nasa/galaxies'> <img className="col s6 icon" src={galaxy} alt="galacy icon"/> </Link>
-                <h4 className="col s4" > Galaxies </h4>
+                <h5 className="col s4" > Galaxies </h5>
                 <Link to='/api/nasa/stars' > <img className="col s6 icon"src={stars} alt="star icon" /></Link>
-                <h4 className="col s4" > Stars </h4>
+                <h5 className="col s4" > Stars </h5>
                 <Link to='/api/nasa/comets'> <img className="col s6 icon"src={comet} alt="comet icon" /> </Link>
-                <h4 className="col s4" > Comets </h4>
-
+                <h5 className="col s4" > Comets </h5>
             </div>
         )
     }
